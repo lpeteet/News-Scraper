ad677ddef92e15fdf3189094fb0117fcd03c3659
Larry Peteet <larrypv10@gmail.com>
2017-10-22 13:30:18 -0400

    Changing Results

---
+++ public/app.js
@@ -0,0 +1,133 @@
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+  // For each one
+  //Clearing Table
+  $('#clienti').bootstrapTable('removeAll');
+  
+  var mydata2 = [];
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    //ORIGINAL WAY: APPENDING ID #articles
+    //$("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    var tmp = [
+      {
+          "id": data[i].id,
+          "title": data[i].title,
+          "link": data[i].link
+      }
+    ];
+    /* SCHOOL GUIDE ORIGINAL CODE
+      var tmp = [
+        {
+          "school.name": name,
+          "id": id,
+          "2014.student.size": studentSize,
+          "2014.admissions.sat_scores.average.overall": sat
+        }
+      ];
+    */
+    mydata2.push.apply(mydata2, tmp);
+  } // FOR LOOP
+
+  $('#clienti').bootstrapTable({
+    undefinedText: 'n/a',
+    data: mydata2
+});
+
+}); // End getJSON("/articles)
+
+//SCHOOL GUIDE ORIGINAL CODE:
+/*
+  //Add Row Click Event to Table
+  $('#clienti').on('click-cell.bs.table', function (field, value, row, $el) {
+      if (value != "type") {
+          // alert($el.id+"-"+$el.name+"-"+$el.type);
+          // alert("Selected Row's ID: '" + $el.id + "'")
+
+          $(".main").css("display", "none");
+          hideWelcome();
+          $(".chart-body").show();
+          DisplayGraphs($el.id);
+          setTimeout(hideLoader, 1000);
+      }
+  });
+
+*/
+
+$('#clienti').on('click-cell.bs.table', function (field, value, row, $el) {
+  if (value != "type") {
+      // alert($el.id+"-"+$el.name+"-"+$el.type);
+      // alert("Selected Row's ID: '" + $el.id + "'")
+
+      //CONSIDER USING SOME LOGIC HERE FROM SCHOOL GUIDE
+      // $(".main").css("display", "none");
+      // hideWelcome();
+      // $(".chart-body").show();
+      // DisplayGraphs($el.id);
+      // setTimeout(hideLoader, 1000);
+  }
+});
+
+// CLASS CODE BEING REPLACED?
+// Whenever someone clicks a p tag
+$(document).on("click", "p", function() {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .done(function(data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
--- server.js
+++ server.js
@@ -1,99 +1,206 @@
-// Dependencies
-var express = require("express");
-var mongojs = require("mongojs");
-
-// Require request and cheerio. This makes the scraping possible
-var request = require("request");
-var cheerio = require("cheerio");
+//  ****************************************************************************
+//  # News-Scraper
+//  NewsScraper App- Homework Week-9 (Thursday)
+//  ****************************************************************************
 
 var PORT = process.env.PORT || 3000;
 
-//MongoDB Access!
-var urlMongoDB = 'mongodb://lpeteet:Raleigh1!@ds121535.mlab.com:21535/news';
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+//  ****************************************************************************
+//  DEPENDENCIES
+//  ****************************************************************************
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+//MongoDB Access!
+//var urlMongoDB = 'mongodb://lpeteet:Raleigh1!@ds121535.mlab.com:21535/news';
+//var databaseUrl = "scraper";
+//var collections = ["scrapedData"];
+
+//  ****************************************************************************
+//  DATABASE CONNECTION
+//  ****************************************************************************
+// var db = mongojs(databaseUrl, collections);
+// db.on("error", function(error) {
+//   console.log("Database Error:", error);
+// });
+
+// Database configuration with mongoose
+//mongoose.connect("mongodb://localhost/week18day3mongoose");
+//var db = mongoose.connection;
+
+// Show any mongoose errors
+//db.on("error", function(error) {
+//  console.log("Mongoose Error: ", error);
+//});
+
+// Once logged in to the db through mongoose, log a success message
+//db.once("open", function() {
+//    console.log("Mongoose connection successful.");
+//  });
+  
+// Initialize Express
 var app = express();
 
-//Database Connection
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/week18Populater", {
+  useMongoClient: true
 });
 
+
+//  ****************************************************************************
+//  ROUTES
+//  ****************************************************************************
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
-    res.send("Hello world");
-  });
+    console.log("Inside app.GET('/')");
+    res.render("index");
+});
   
-// Retrieve data from the db
+//  ----------------------------------------------------------------------------
+//  "'/ALL" Route
+//  ----------------------------------------------------------------------------
 app.get("/all", function(req, res) {
+    console.log("Inside app.GET('/all')");
     // Find all results from the scrapedData collection in the db
-    db.scrapedData.find({}, function(error, found) {
+    db.Article.find({}, function(error, found) {
       // Throw any errors to the console
       if (error) {
-        console.log(error);
+        console.log("Error in Get('all'):", error);
       }
       // If there are no errors, send the data to the browser as json
       else {
         res.json(found);
       }
     });
-  });
-  
-  
-  
-  // Retrieve data from the db
-app.get("/all", function(req, res) {
-    // Find all results from the scrapedData collection in the db
-    db.scrapedData.find({}, function(error, found) {
-      // Throw any errors to the console
-      if (error) {
-        console.log(error);
-      }
-      // If there are no errors, send the data to the browser as json
-      else {
-        res.json(found);
-      }
-    });
-  });
-
-// Scrape Data
-app.get("/scrape", function(req, res) {
-    // Make a request for the news section of ycombinator
-    request("https://news.ycombinator.com/", function(error, response, html) {
-      // Load the html body from request into cheerio
-      var $ = cheerio.load(html);
-      // For each element with a "title" class
-      $(".title").each(function(i, element) {
-        // Save the text and href of each link enclosed in the current element
-        var title = $(element).children("a").text();
-        var link = $(element).children("a").attr("href");
-  
-        // Only Insert if we have Title and Link!
-        if (title && link) {
-          // Actual DB Insert
-          db.scrapedData.insert({
-            title: title,
-            link: link
-          },
-          //Error Function from DB Insert
-          function(err, inserted) {
-            if (err) {
-              console.log(err);
-            }
-            else {
-              //All Good!
-              console.log(inserted);
-            }
-          });
-        }
-      });
-    });
-  
-  app.listen(PORT, function() {
-    console.log("News-Scraper listening on http://localhost:" + PORT);
 });
 
+//  ----------------------------------------------------------------------------
+//  "'/scrape" Route
+//  ----------------------------------------------------------------------------
+app.get("/scrape", function(req, res) {
+    console.log("Inside app.GET('/scrape')");
+    // First, we grab the body of the html with request
+  axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+        console.log("SCRAPING, RESULT:", result);
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          // If we were able to successfully scrape and save an Article, send a message to the client
+          //res.send("Scrape Complete");
+          res.redirect("/all");
+        })
+        .catch(function(error) {
+            console.log("Error in Get('/Scrape'):", error);
+            // If an error occurred, send it to the client
+          res.json(error);
+        });
+    });
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    console.log("Inside app.GET('/articles')");
+    // Grab every document in the Articles collection
+  db.Article
+    .find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(error) {
+        console.log("Error in Get('/articles'):", error);
+        // If an error occurred, send it to the client
+      res.json(error);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+    console.log("Inside app.GET('/articles/:id')");
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article
+    .findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(error) {
+        console.log("Error in Get('/articles/:id'):", error);
+        // If an error occurred, send it to the client
+      res.json(error);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+    console.log("Inside app.POST('/articles/:id')");
+    // Create a new note and pass the req.body to the entry
+  db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(error) {
+        console.log("Error in POST('/articles/:id'):", error);
+        // If an error occurred, send it to the client
+      res.json(error);
+    });
+});
+
+app.listen(PORT, function() {
+    console.log("News-Scraper listening on http://localhost:" + PORT);
+});
 
 
